6e8189a56f32865400c318aa1e2b4ee7b508c3e2
Ian Moore <ianmooregis@gmail.com>
2016-12-24 13:24:42 -0600

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -8,8 +9,8 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
-    //console.log(req.body);
     var todo = new Todo({
        text: req.body.text 
     });
@@ -26,6 +27,29 @@ app.get('/todos', (req,res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+
+//Allows for dynamic url, such as GET /todos/1234
+//creates id variable, found on the request object
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    //checks for valid ID
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    //if ID is valid but not found, then 404 is sent
+    //if ID is valid and found, then object is sent
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
