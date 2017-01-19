4794c2742d243456a918f154fdcf276d6e4fb0fc
Ian Moore <ianmooregis@gmail.com>
2017-01-18 18:50:06 -0600

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ const port = process.env.PORT; //port used for Heroku, otherwise 3000
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-       text: req.body.text 
+        text: req.body.text,
+        _creator: req.user._id
     });
     todo.save().then((doc) => {
         res.send(doc);
@@ -27,8 +28,10 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req,res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req,res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
@@ -37,7 +40,7 @@ app.get('/todos', (req,res) => {
 
 //allows for dynamic url, such as GET /todos/1234
 //creates ID variable, found on the request object
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     //checks for valid ID
     if (!ObjectID.isValid(id)) {
@@ -46,7 +49,10 @@ app.get('/todos/:id', (req, res) => {
     //if ID is valid but not found, then 404 is sent
     //if ID is valid and found, then object is sent
     //if ID is invalid, then 400 is sent
-    Todo.findById(id).then((todo) => {
+    Todo.findOne({
+        _id: id,
+        _creator: req.user.id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -56,12 +62,15 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }    
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id    
+    }).then((todo) => {
        if (!todo) {
            return res.status(404).send();
        }
@@ -71,7 +80,7 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']); //allows picking individual properties on an object
     
@@ -84,7 +93,7 @@ app.patch('/todos/:id', (req, res) => {
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
